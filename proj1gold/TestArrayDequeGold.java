cf1e9a4d8750fdc1a35d9903385be726fecaf74e
yuuuuu422 <564183039@qq.com>
2022-01-22 00:14:27 +0800

    proj1gold fix

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -3,41 +3,43 @@ import org.junit.Test;
 
 public class TestArrayDequeGold {
     @Test
-    public void testArrayDequeGold(){
+    public void testArrayDequeGold() {
         ArrayDequeSolution<Integer> arrayDequeSolution = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> studentArrayDeque = new StudentArrayDeque<>();
-        for(int i=0;i<1000;i++){
-            if (arrayDequeSolution.size()==0){
+        for (int i = 0; i < 1000; i++) {
+            if (arrayDequeSolution.size() == 0) {
                 int opt_2 = StdRandom.uniform(2);
-                if (opt_2 == 0){
+                if (opt_2 == 0) {
                     arrayDequeSolution.addFirst(i);
                     studentArrayDeque.addFirst(i);
-                }else {
-                    Integer arrRemoveNumber = null,stuRemoveNumber=null;
-                    int opt_4 = StdRandom.uniform(4);
-                    switch (opt_4){
-                        case 0:
-                            arrayDequeSolution.addFirst(i);
-                            studentArrayDeque.addFirst(i);
-                            break;
-                        case 1:
-                            arrayDequeSolution.addLast(i);
-                            studentArrayDeque.addLast(i);
-                            break;
-                        case 2:
-                            arrRemoveNumber= arrayDequeSolution.removeFirst();
-                            stuRemoveNumber= studentArrayDeque.removeFirst();
-                            break;
-                        case 3:
-                            arrRemoveNumber = arrayDequeSolution.removeLast();
-                            stuRemoveNumber = studentArrayDeque.removeLast();
-                            break;
-                        default:
-                    }
-                    assertEquals("",arrRemoveNumber,stuRemoveNumber);
+                } else {
+                    arrayDequeSolution.addLast(i);
+                    studentArrayDeque.addLast(i);
                 }
+            } else {
+                Integer arrRemoveNumber = null, stuRemoveNumber = null;
+                int opt_4 = StdRandom.uniform(4);
+                switch (opt_4) {
+                    case 0:
+                        arrayDequeSolution.addFirst(i);
+                        studentArrayDeque.addFirst(i);
+                        break;
+                    case 1:
+                        arrayDequeSolution.addLast(i);
+                        studentArrayDeque.addLast(i);
+                        break;
+                    case 2:
+                        arrRemoveNumber = arrayDequeSolution.removeFirst();
+                        stuRemoveNumber = studentArrayDeque.removeFirst();
+                        break;
+                    case 3:
+                        arrRemoveNumber = arrayDequeSolution.removeLast();
+                        stuRemoveNumber = studentArrayDeque.removeLast();
+                        break;
+                    default:
+                }
+                assertEquals("", arrRemoveNumber, stuRemoveNumber);
             }
         }
-
     }
 }
